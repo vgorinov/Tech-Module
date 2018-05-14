838f3ee75b5a1f514e1a1f81e339275271f8b78b
Victor Gorinov <35743237+vgorinov@users.noreply.github.com>
2018-05-15 00:24:22 +0300

    Add files via upload

---
+++ Basic Syntax Lab/02_AddTwoNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int sum = a + b;
+            Console.WriteLine($"{a} + {b} = {sum}");
+        }
+    }
+}
