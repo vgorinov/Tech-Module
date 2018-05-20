9169a121beeab3408cd04ef5faf082a11cf80974
Victor Gorinov <vgorinov@abv.bg>
2018-05-20 17:12:25 +0300

    Conditional Statements and Loops Exercises

---
+++ 02_Conditional_Statements_And_Loops_Exercises/14_MagicLetter/14_MagicLetter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _14_MagicLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstLetter = char.Parse(Console.ReadLine());
+            char secondLetter = char.Parse(Console.ReadLine());
+            string thirdLetter = Console.ReadLine();
+
+            string result = "";
+
+            for (char d1 = firstLetter; d1 <= secondLetter; d1++)
+            {
+                for (char d2 = firstLetter; d2 <= secondLetter; d2++)
+                {
+                    for (char d3 = firstLetter; d3 <= secondLetter; d3++)
+                    {
+                        result = $"{d1}{d2}{d3} ";
+
+                        if (!result.Contains(thirdLetter))
+                        {
+                            Console.Write(result);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
