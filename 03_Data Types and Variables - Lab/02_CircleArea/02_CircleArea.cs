a7abc4c855cf6b17cb0b540e9e1448ba76c45398
Victor Gorinov <vgorinov@abv.bg>
2018-05-25 17:42:10 +0300

    Data Types and Variables - Lab

---
+++ 03_Data Types and Variables - Lab/02_CircleArea/02_CircleArea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02_CircleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:f12}", Math.PI * r * r);
+        }
+    }
+}
