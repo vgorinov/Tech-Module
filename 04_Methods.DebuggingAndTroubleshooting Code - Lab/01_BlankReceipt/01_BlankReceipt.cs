83b8423061ba77fe2a2aadf1532bef3c370f0799
Victor Gorinov <vgorinov@abv.bg>
2018-05-28 00:09:31 +0300

    04_Methods. Debugging And Troubleshooting Code_Lab

---
+++ 04_Methods.DebuggingAndTroubleshooting Code - Lab/01_BlankReceipt/01_BlankReceipt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _01_BlankReceipt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintReceipt();
+        }
+
+
+
+        static void Header()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+
+
+        static void Body()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+
+
+        static void Footer()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("© SoftUni");
+        }
+
+
+        static void PrintReceipt()
+        {
+            Header();
+            Body();
+            Footer();
+        }
+    }
+}
---
+++ 04_Methods.DebuggingAndTroubleshooting Code - Lab/04_DrawAFilledSquare/04_DrawAFilledSquare.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04_DrawAFilledSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintDashes(n);
+            PrintBody(n);
+            PrintDashes(n);
+        }
+
+
+        static void PrintDashes(int n)
+        {
+            Console.WriteLine(new string('-', n * 2));
+        }
+
+        static void PrintBody(int n)
+        {
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write("-");
+
+                for (int j = 0; j < n - 1; j++)
+                {
+                    Console.Write("\\/");
+                }
+
+                Console.WriteLine("-");
+            }
+        }
+    }
+}
+
