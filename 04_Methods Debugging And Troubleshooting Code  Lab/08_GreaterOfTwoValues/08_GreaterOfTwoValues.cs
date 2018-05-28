36bc62c8a52171ae2230d2c3d2ad122a86f39e01
Victor Gorinov <vgorinov@abv.bg>
2018-05-28 23:13:55 +0300

    Methods - Problem 08 - Greater Of Two Values

---
+++ 04_Methods Debugging And Troubleshooting Code  Lab/08_GreaterOfTwoValues/08_GreaterOfTwoValues.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace _08_GreaterOfTwoValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            if (type == "int")
+            {
+                int first = int.Parse(Console.ReadLine());
+                int second = int.Parse(Console.ReadLine());
+                int result = GetMax(first, second);
+                Console.WriteLine(result);
+            }
+            else if (type == "char")
+            {
+                char first = char.Parse(Console.ReadLine());
+                char second = char.Parse(Console.ReadLine());
+                char result = GetMax(first, second);
+                Console.WriteLine(result);
+            }
+            else
+            {
+                string first = Console.ReadLine();
+                string second = Console.ReadLine();
+                string result = GetMax(first, second);
+                Console.WriteLine(result);
+            }
+        }
+
+
+        static int GetMax(int first, int second)
+        {
+            if (first > second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+
+        static char GetMax(char first, char second)
+        {
+            if (first > second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+
+        static string GetMax(string first, string second)
+        {
+            if (first.CompareTo(second) > 0)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+    }
+}
