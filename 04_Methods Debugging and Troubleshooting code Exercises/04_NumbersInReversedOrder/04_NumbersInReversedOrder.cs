4ea4fb63d27e448ca354ae07d233ed8b711ad045
Victor Gorinov <vgorinov@abv.bg>
2018-05-30 17:06:11 +0300

    Update

---
+++ 04_Methods Debugging and Troubleshooting code Exercises/04_NumbersInReversedOrder/04_NumbersInReversedOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _04_NumbersInReversedOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberString = Console.ReadLine();
+
+            ReverseNumbers(numberString);
+        }
+
+        static void ReverseNumbers(string numStr)
+        {
+            string result = "";
+            char symbol = '\0';
+            for (int i = numStr.Length - 1; i >= 0; i--)
+            {
+                symbol = numStr[i];
+                result += symbol;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 04_Methods Debugging and Troubleshooting code Exercises/05_FibonacciNumbers/05_FibonacciNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05_FibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else if (number == 1)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine(GetFibonacciNumbers(number));
+            }
+        }
+
+
+        static int GetFibonacciNumbers(int n)
+        {
+            int a = 0;
+            int b = 1;
+            int c = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            return c;
+        }
+    }
+}
