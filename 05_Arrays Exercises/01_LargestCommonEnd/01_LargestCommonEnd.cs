298cad72c7672899f7559a28dd28e9383894ae8c
Victor Gorinov <vgorinov@abv.bg>
2018-06-04 00:32:07 +0300

    Arrays - Exercises

---
+++ 05_Arrays Exercises/01_LargestCommonEnd/01_LargestCommonEnd.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_LargestCommonEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split().ToArray();
+            string[] secondArray = Console.ReadLine().Split().ToArray();
+
+            var shorterArray = Math.Min(firstArray.Length, secondArray.Length);
+            var longerArray = Math.Max(firstArray.Length, secondArray.Length);
+
+            int countLeft = 0;
+            int countRight = 0;
+            
+            for (int i = 0; i < shorterArray; i++)
+            {
+                if (firstArray[i] == secondArray[i])
+                {
+                    countLeft++;
+                }
+
+                if (firstArray[firstArray.Length - 1 - i] == secondArray[secondArray.Length - 1 - i])
+                {
+                    countRight++;
+                }
+            }
+
+            int countTotal = Math.Max(countLeft, countRight);
+            Console.WriteLine(countTotal);
+        }
+    }
+}
