0dceae46b87da8b539868208eeed8dbf38b73b5a
Victor Gorinov <vgorinov@abv.bg>
2018-06-05 22:50:50 +0300

    Update

---
+++ 05_Arrays Lab/06_ReverseAnArrayOfStrings/06_ReverseAnArrayOfStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _06_ReverseAnArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(' ').ToArray();
+
+            // Can use Array.Reverse(); for shorter syntax
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                string temporary = array[i];
+                array[i] = array[array.Length - 1 - i];
+                array[array.Length - 1 - i] = temporary;
+            }
+
+            Console.WriteLine(String.Join(" ", array));
+        }
+    }
+}
