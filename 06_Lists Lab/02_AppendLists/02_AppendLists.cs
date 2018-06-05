21f6e0eda86c4a815897e65f669a4dac957681b4
Victor Gorinov <vgorinov@abv.bg>
2018-06-05 23:02:43 +0300

    Lists - Lab

---
+++ 06_Lists Lab/02_AppendLists/02_AppendLists.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_AppendLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split('|').ToArray();
+            List<string> result = new List<string>();
+            Array.Reverse(input);
+
+            foreach (var token in input)
+            {
+                string[] numbers = token.Split(' ');
+
+                foreach (var item in numbers)
+                {
+                    if (item != " ")
+                    {
+                        result.Add(item);
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", result));
+        }
+    }
+}
