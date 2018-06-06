8015f4012f45b044bbbcee07bbd132a2eafd0290
Victor Gorinov <vgorinov@abv.bg>
2018-06-06 23:30:23 +0300

    Arrays Lab update

---
+++ 05_Arrays Lab/07_SumArrays/07_SumArrays.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _07_SumArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int largerLength = Math.Max(firstArray.Length, secondArray.Length);
+
+            for (int i = 0; i < largerLength; i++)
+            {
+                int firstValue = firstArray[i % firstArray.Length];
+                int secondValue = secondArray[i % secondArray.Length];
+                Console.Write("{0} ", firstValue + secondValue);
+            }
+
+        }
+    }
+}
