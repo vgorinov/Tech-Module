82b531ede8747049ee61e53a02da8996511d490d
Victor Gorinov <vgorinov@abv.bg>
2018-06-08 22:21:14 +0300

    Update

---
+++ 05_Arrays Exercises/03_FoldAndSum/03_FoldAndSum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_FoldAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int k = arr.Length / 4;
+
+            int[] left = GetPart(arr, k, 0);
+            int[] middle = GetPart(arr, k * 2, k);
+            int[] right = GetPart(arr, k, k * 3);
+
+            Array.Reverse(left);
+            Array.Reverse(right);
+
+            int[] merged = new int[k * 2];
+
+            int index = 0;
+            for (int i = 0; i < k; i++)
+            {
+                merged[index++] = left[i];
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                merged[index++] = right[i];
+            }
+
+            for (int i = 0; i < middle.Length; i++)
+            {
+                int sum = middle[i] + merged[i];
+                Console.Write($"{sum} ");
+            }
+            Console.WriteLine();
+
+        }
+
+        private static int[] GetPart(int[] arr, int size, int startIndex)
+        {
+            int[] part = new int[size];
+            int index = 0;
+            for (int i = startIndex; i < size + startIndex; i++)
+            {
+                part[i - startIndex] = arr[i];
+            }
+
+            return part;
+        }
+    }
+}
---
+++ 05_Arrays Exercises/05_CompareCharArrays/05_CompareCharArrays.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _05_CompareCharArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] firstArray = Console.ReadLine().Split().Select(char.Parse).ToArray();
+            char[] secondArray = Console.ReadLine().Split().Select(char.Parse).ToArray();
+
+            if (firstArray.Length == secondArray.Length)
+            {
+
+                if (firstArray[0] < secondArray[0])
+                {
+                    Console.WriteLine(firstArray);
+                    Console.WriteLine(secondArray);
+                }
+                else
+                {
+                    Console.WriteLine(secondArray);
+                    Console.WriteLine(firstArray);
+                }
+            }
+
+            else if (firstArray.Length < secondArray.Length)
+            {
+                Console.WriteLine(firstArray);
+                Console.WriteLine(secondArray);
+            }
+            else
+            {
+                Console.WriteLine(secondArray);
+                Console.WriteLine(firstArray);
+            }
+        }
+    }
+}
