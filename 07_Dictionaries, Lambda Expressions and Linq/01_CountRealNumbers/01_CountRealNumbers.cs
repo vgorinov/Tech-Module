cf8e1510cc6d0a33e87ff15ae5907e9be2039693
Victor Gorinov <vgorinov@abv.bg>
2018-06-11 23:57:15 +0300

    Dictionaries, Lambda Expressions and Linq - Lab

---
+++ 07_Dictionaries, Lambda Expressions and Linq/01_CountRealNumbers/01_CountRealNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq.Expressions;
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            var counts = new SortedDictionary<double, int>();
+
+            foreach (var num in input)
+            {
+                double parsedNum = double.Parse(num);
+                if (counts.ContainsKey(parsedNum))
+                {
+                    counts[parsedNum]++;
+                }
+                else
+                {
+                    counts[parsedNum] = 1;
+                }
+            }
+
+            foreach (var num in counts.Keys)
+            {
+                Console.WriteLine("{0} -> {1}", num, counts[num]);
+            }
+
+        }
+
+    }
+
+}
